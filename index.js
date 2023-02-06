a6428a5cae15f2a3c10be31a89bbf45ea2f68ea0
Sohini Bhattacharya <bhattacharyasohini48@gmail.com>
2023-02-06 13:46:46 +0530

    Added apis for single post and edit post

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const fs = require('fs');
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 const secret = process.env.SECRET; 
 const salt= bcrypt.genSaltSync(10);
@@ -66,6 +67,8 @@ app.get('/profile', (req,res) => {
 app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
+
+
 app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
   const {originalname,path} = req.file;
   const parts = originalname.split('.');
@@ -102,6 +105,37 @@ app.get('/post/:id', async (req, res) => {
   const {id} = req.params;
   const postDoc = await Post.findById(id).populate('author', ['username']);
   res.json(postDoc);
+});
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath  ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
 });
 
 
